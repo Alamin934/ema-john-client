2bc5f193cfb90e32a6a12e6eb0f66045a5b7f245
Alamin934 <islamalamin934@gmail.com>
2022-06-20 12:30:07 +0600

    secure user order route and set status for unAothurized user and redirect user to login page

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
     const { user } = useAuth();
-
+    let navigate = useNavigate();
     useEffect(() => {
         fetch(`http://localhost:5000/orders?email=${user.email}`, {
             headers: { 'authorization': `Bearar ${localStorage.getItem('idToken')}` }
         })
-            .then(res => res.json())
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json()
+                }
+                else if (res.status === 401) {
+                    navigate('/login');
+                }
+            })
             .then(data => setOrders(data))
-    }, [user.email])
+    }, [user.email, navigate]);
 
     return (
         <div style={{ textAlign: 'center' }}>
